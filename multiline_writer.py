0270d33ba3d96a76e393f56fcb714da3b62e705f
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-04-27 18:57:33 +0800

    pseudocode

---
+++ multiline_writer.py
@@ -0,0 +1,22 @@
+# Import PySimpleGUI module
+import PySimpleGUI
+# Open the file "mylife.txt" in write mode
+
+# Define the function for entering lines
+
+    # Open the file "mylife.txt" in append mode
+
+    # Prompt the user to enter a line
+    # If the user clicks the "Add this line" button, the entered line will be appended to "mylife.txt"
+    # If the user clicks the "Exit" button, the program will terminate
+
+# Define the layout for the main window
+# Create the main window 
+# Call the function for entering lines
+
+# Define the layout for the "Add Line" window
+# Create the "Add Line" window
+# Create a loop to continuously prompt the user to add more lines
+    # If the user clicks the "No" button, the program will terminate
+    # If the user clicks the "Yes" button, the function for entering lines will be called
+    # If the user clicks the "Save File" button, all the entered lines will be saved to "mylife.txt"
